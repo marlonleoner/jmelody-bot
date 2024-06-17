d88097fdbcd63ddbbdb3edd353dc6a6f8642349b
Marlon Leoner <marlon.leoner@gmail.com>
2024-06-17 00:08:16 -0300

    others embed models implemented

--- src/main/java/me/leoner/jmelody/bot/service/EmbedGenerator.java
+++ src/main/java/me/leoner/jmelody/bot/service/EmbedGenerator.java
@@ -1,6 +1,10 @@
 package me.leoner.jmelody.bot.service;
 
+import com.sedmelluq.discord.lavaplayer.track.AudioPlaylist;
+import com.sedmelluq.discord.lavaplayer.track.AudioTrack;
+import com.sedmelluq.discord.lavaplayer.track.AudioTrackInfo;
 import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.entities.Member;
 import net.dv8tion.jda.api.entities.MessageEmbed;
 
 import java.time.Instant;
@@ -11,6 +15,14 @@ public class EmbedGenerator {
 
     private EmbedGenerator() {
         // empty constructor
+    }
+
+    private static String formatDuration(long duration) {
+        long second = (duration / 1000) % 60;
+        long minute = (duration / (1000 * 60)) % 60;
+        long hour = (duration / (1000 * 60 * 60)) % 24;
+
+        return String.format("%02d:%02d:%02d", hour, minute, second);
     }
 
     private static EmbedBuilder createBase(boolean timestamp) {
@@ -32,6 +44,28 @@ public class EmbedGenerator {
 
     public static MessageEmbed withErrorMessage(String message) {
         return EmbedGenerator.withMessage(":cross: " + message);
+    }
+
+    public static MessageEmbed withTrackAdded(AudioTrack track, Member member) {
+        AudioTrackInfo infos = track.getInfo();
+
+        EmbedBuilder embed = EmbedGenerator.createBase(true);
+
+        embed.addField("Track Queued", "`#1` • [`" + infos.title + "`](" + infos.uri + ")", true);
+        embed.addField("Request by", member.getAsMention(), true);
+        embed.addField("Duration", "`" + EmbedGenerator.formatDuration(infos.length) + "`", true);
+
+        return embed.build();
+    }
+
+    public static MessageEmbed withPlaylistAdded(AudioPlaylist playlist, Member member) {
+        EmbedBuilder embed = EmbedGenerator.createBase(true);
+
+        embed.addField("Playlist Queued", "`" + playlist.getName() + "`", true);
+        embed.addField("Request by", member.getAsMention(), true);
+        embed.addField("Total Tracks", "`" + playlist.getTracks().size() + " song(s)`", true);
+
+        return embed.build();
     }
 
 }
